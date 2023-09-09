4abf5a489dc45c0509c4f82fb8bed2f4e513d1d7
Leelakrishnamohanbolla7711 <142087484+Leelakrishnamohanbolla7711@users.noreply.github.com>
2023-09-09 15:31:52 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class a{
+public static void main(String[] args){
+int x;
+String y;
+Double z;
+Scanner sc=new Scanner(System.in);
+x=sc.nextInt();
+y=sc.nextLine();
+z=sc.nextDouble();
+System.out.println(x +" "+y +" "+z);
+}
+}
